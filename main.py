b36b4b6c54f4b955efd5de6b846762ed1d71938c
DadMych <60585194+DadMych@users.noreply.github.com>
2021-10-14 23:28:35 +0300

    +added webhooks, bugfixes

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_message():
 def index():
     bot.remove_webhook()
     bot.set_webhook(url="https://{}.herokuapp.com/{}".format(APP_NAME, TOKEN))
-    return "Hello from Heroku!", 200
+    return "QuestioningBot живет тут!", 200
 
 
 if __name__ == "__main__":
